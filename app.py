e4eb808f73543b0ba0ccb10ef7a59676fea44e94
Miro Gardiola <st25209@ormiston.school.nz>
2026-08-21 10:08:50 +1200

    Imports the JSON data and passes it on to HTML

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+import json
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+
+def load_pizza_data():
+    with open('data/pizzas1.json') as file:
+        pizzas1 = json.load(file)
+        return pizzas1
 
 @app.route('/')
 def index():
