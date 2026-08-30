d5dfcac4d41384fc1f2efee1cf4467754d19cffb
Miro Gardiola <st25209@ormiston.school.nz>
2026-08-31 00:07:45 +1200

    Function renders About Page

--- app.py
+++ app.py
@@ -19,5 +19,9 @@ def index():
 def home():
     return render_template("sample.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
